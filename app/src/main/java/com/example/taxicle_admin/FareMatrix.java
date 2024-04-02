fc569e2f21fa0bef9c3963df6364e73c1c7a988b
joneltech07 <jonel.uligan7@gmail.com>
2024-04-03 01:32:20 +0800

    Added Phone limit Fare Matrix

--- app/src/main/java/com/example/taxicle_admin/FareMatrix.java
+++ app/src/main/java/com/example/taxicle_admin/FareMatrix.java
@@ -21,43 +21,11 @@ import java.util.Map;
 
 public class FareMatrix extends AppCompatActivity {
 
-    EditText value;
-
-    Button update;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fare_matrix);
 
-        value = findViewById(R.id.txtValue);
-
-        FirebaseDatabase.getInstance().getReference(Fare.class.getSimpleName())
-                .addValueEventListener(new ValueEventListener() {
-                    @SuppressLint("SetTextI18n")
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if (snapshot.exists()) {
-                            Fare fare = snapshot.getValue(Fare.class);
-                            assert fare != null;
-                            value.setText(Double.toString(fare.getValue()));
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-
-        update = findViewById(R.id.btn_update);
-        update.setOnClickListener(v -> {
-            Map<String, Object> map = new HashMap<>();
-            map.put("value", Double.parseDouble(value.getText().toString()));
-
-            FirebaseDatabase.getInstance().getReference(Fare.class.getSimpleName())
-                    .updateChildren(map).addOnSuccessListener(unused -> Toast.makeText(FareMatrix.this, "You've successfully updated the fare matrix", Toast.LENGTH_SHORT).show());
-        });
 
     }
 }
